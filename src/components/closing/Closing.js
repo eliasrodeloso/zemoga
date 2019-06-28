14a42b197e291b2854c92fc6841f77ab52887b09
Elias Rodelo <eliasrodeloso@gmail.com>
2019-06-28 14:25:32 -0500

    Closing component finished

--- src/components/closing/Closing.js
+++ src/components/closing/Closing.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Closing.scss';
 
-const Closing = () => (
+const Closing = ({ daysLeft }) => (
   <div className="Closing">
     <div className="Closing-bar">
-      <div className="Closing-progress">
+      <div
+        className="Closing-progress"
+        style={{ width: `${((30 - daysLeft) * 100) / 30}%` }}
+      >
         <span className="Closing-progress-text">Closing in</span>
       </div>
       <div className="Closing-progress-left">
         <span className="Closing-progress-text--left">
-          <span className="Closing-progress-text--number">22</span>
+          <span className="Closing-progress-text--number">{daysLeft}</span>
           {' '}
 days
         </span>
@@ -17,5 +21,13 @@ days
     </div>
   </div>
 );
+
+Closing.propTypes = {
+  daysLeft: PropTypes.number
+};
+
+Closing.defaultProps = {
+  daysLeft: 22
+};
 
 export default Closing;
