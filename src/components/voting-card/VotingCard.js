72f57f78efb1d8309d791c311439559a1775bea3
Elias Rodelo <eliasrodeloso@gmail.com>
2019-06-29 17:55:24 -0500

    Interaction on votes added

--- src/components/voting-card/VotingCard.js
+++ src/components/voting-card/VotingCard.js
@@ -6,48 +6,58 @@ import { ReactComponent as WikiIcon } from '../../assets/icons/wiki.svg';
 import './VotingCard.scss';
 
 const VotingCard = ({
-  title, description, informationLink, actions
-}) => (
-  <div className="VotingCard">
-    <div className="VotingCard-crop" />
-    <div className="VotingCard-wrapper">
-      <header className="VotingCard-header">
-        <h4>What&apos;s your opinion on</h4>
-        <h1>{title}</h1>
-      </header>
-      <section className="VotingCard-content">
-        <p>{description}</p>
-        <div className="VotingCard-more">
-          <WikiIcon />
-          <a href={informationLink}>More information</a>
-        </div>
-        <h2>What&apos;s your veredict?</h2>
-      </section>
-      <footer className="VotingCard-footer">
-        <button
-          type="button"
-          className="VotingCard-choice VotingCard-choice--up"
-          onClick={() => actions.voteUp(title)}
-        >
-          <ThumnsUpIcon />
-        </button>
-        <button
-          type="button"
-          className="VotingCard-choice VotingCard-choice--down"
-          onClick={() => actions.voteDown(title)}
-        >
-          <ThumnsDownIcon />
-        </button>
-      </footer>
+  title,
+  description,
+  informationLink,
+  actions,
+  store
+}) => {
+  const totalVotes = store.votesUp + store.votesDown;
+  return (
+    <div className="VotingCard">
+      <div className="VotingCard-crop" />
+      <div className="VotingCard-wrapper">
+        <header className="VotingCard-header">
+          <h4>What&apos;s your opinion on</h4>
+          <h1>{title}</h1>
+        </header>
+        <section className="VotingCard-content">
+          <p>{description}</p>
+          <div className="VotingCard-more">
+            <WikiIcon />
+            <a href={informationLink}>More information</a>
+          </div>
+          <h2>What&apos;s your veredict?</h2>
+        </section>
+        <footer className="VotingCard-footer">
+          <button
+            type="button"
+            className="VotingCard-choice VotingCard-choice--up"
+            onClick={() => actions.voteUp(title)}
+            style={{ width: `${(store.votesUp * 100) / totalVotes}%` }}
+          >
+            <ThumnsUpIcon />
+          </button>
+          <button
+            type="button"
+            className="VotingCard-choice VotingCard-choice--down"
+            onClick={() => actions.voteDown(title)}
+            style={{ width: `${(store.votesDown * 100) / totalVotes}%` }}
+          >
+            <ThumnsDownIcon />
+          </button>
+        </footer>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 VotingCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   informationLink: PropTypes.string.isRequired,
-  actions: PropTypes.objectOf(PropTypes.func).isRequired
+  actions: PropTypes.objectOf(PropTypes.func).isRequired,
+  store: PropTypes.objectOf(PropTypes.any).isRequired
 };
 
 export default VotingCard;
--- src/containers/voting-card/VotingCard.container.js
+++ src/containers/voting-card/VotingCard.container.js
@@ -3,6 +3,12 @@ import { bindActionCreators } from 'redux';
 import VotesActions from '../../core/actions/votes.action';
 import VotingCard from '../../components/voting-card/VotingCard';
 
+const mapStateToProps = state => ({
+  store: {
+    ...state.votesStore
+  }
+});
+
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(
     { voteUp: VotesActions.votesUp, voteDown: VotesActions.votesDown },
@@ -11,13 +17,17 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const mapOwnProps = (stateProps, dispatchProps, ownProps) => ({
-  ...stateProps,
   ...dispatchProps,
-  ...ownProps
+  ...ownProps,
+  store: {
+    ...stateProps.store.votedRulers.filter(
+      votedRuler => votedRuler.rulerName === ownProps.title
+    )[0]
+  }
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
   mapOwnProps
 )(VotingCard);
